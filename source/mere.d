5b4def91d6a2a3d3e8239479795dd00ff4a8cd0c
Filipp Chertiev <f.chertiev@lenvendo.ru>
2022-01-22 21:39:48 +0000

    Обновить source/mere.d

--- source/mere.d
+++ source/mere.d
@@ -6,16 +6,11 @@ import vibe.core.log : logDebug, logInfo, logError, logException;
 import dyaml;
 import deepath.helpers;
 
-template Singleton()
-{
-    static typeof(this) one()
-    {
-        if (!isInstanced)
-        {
-            synchronized(typeof(this).classinfo)
-            {
-                if (!instance)
-                {
+template Singleton() {
+    static typeof(this) one() {
+        if (!isInstanced) {
+            synchronized(typeof(this).classinfo) {
+                if (!instance) {
                     instance = new typeof(this)();
                 }
                 isInstanced = true;
@@ -28,19 +23,16 @@ template Singleton()
     private __gshared typeof(this) instance;
 }
 
-class mereStash
-{
+class mereStash {
     mixin Singleton;
     public HTTPServerSettings httpServerSettings;
     public URLRouter router;
     public Node[string] endpoints;
 
     public this() {}
-    public bool doInit() @safe
-    {
+    public bool doInit() @safe {
         debug { mixin(logFunctionBorders!()); }
         this.httpServerSettings = new HTTPServerSettings;
-//        this.httpClientSettings = new HTTPClientSettings;
         this.router = new URLRouter;
 
         return true;
